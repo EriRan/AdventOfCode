2e5ee2675ca9a620a27e27d7570ba39f00bd5683
Erik <erik.rantanen@hotmail.fi>
2020-12-04 19:46:02 +0200

    Change to algorithm that tries all combinations

--- src/main/java/fi/eriran/day1/ReportRepair.java
+++ src/main/java/fi/eriran/day1/ReportRepair.java
@@ -1,26 +1,20 @@
 package fi.eriran.day1;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static fi.eriran.day1.constant.ReportConstant.NUMBER_TO_FIND;
 
 class ReportRepair {
 
     public int repair(List<Integer> input) {
-        List<Integer> sortedInput = input.stream().sorted(Integer::compareTo).collect(Collectors.toList());
-
-        int leftPointer = 0;
-        int rightPointer = sortedInput.size() - 1;
-
-        while (leftPointer < rightPointer) {
-            Integer leftNumber = sortedInput.get(leftPointer);
-            Integer rightNumber = sortedInput.get(rightPointer);
-            if (leftNumber + rightNumber == NUMBER_TO_FIND) {
-                return leftNumber * rightNumber;
+        for (int leftPointer = 0; leftPointer < input.size(); leftPointer++) {
+            for (int rightPointer = input.size() - 1; rightPointer > leftPointer; rightPointer--) {
+                Integer leftNumber = input.get(leftPointer);
+                Integer rightNumber = input.get(rightPointer);
+                if (leftNumber + rightNumber == NUMBER_TO_FIND) {
+                    return leftNumber * rightNumber;
+                }
             }
-            leftPointer++;
-            rightPointer--;
         }
         throw new IllegalArgumentException("Unable to find numbers that add up to " + NUMBER_TO_FIND + "!");
     }
