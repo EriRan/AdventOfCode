5c2ca648be65450fd32f5a70d6c86260b05c1e0f
Erik <erik.rantanen@hotmail.fi>
2020-12-08 21:22:24 +0200

    Add day 8 part one logic

--- src/main/java/fi/eriran/day8/HandheldHaltingMain.java
+++ src/main/java/fi/eriran/day8/HandheldHaltingMain.java
@@ -1,4 +1,22 @@
 package fi.eriran.day8;
 
+import fi.eriran.common.parser.LineInputParserProxy;
+import fi.eriran.day8.parser.InstructionParser;
+import fi.eriran.day8.pojo.Instruction;
+import fi.eriran.day8.runtime.InstructionRunner;
+
+import java.util.List;
+
 public class HandheldHaltingMain {
+
+    public static void main(String[] args) {
+        List<Instruction> instructions = new InstructionParser().parse(
+                new LineInputParserProxy().parse("day8Input")
+        );
+        System.out.println("Part one accumulator at infinite loop start: "
+                + new InstructionRunner()
+                .findAccumulatorValueAtInfiniteLoop(instructions)
+                .getFinalAccumulatorValue()
+        );
+    }
 }
