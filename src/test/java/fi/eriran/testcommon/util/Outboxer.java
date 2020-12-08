733a2747b962d215f27d3c2265407dc476ca594d
Erik <erik.rantanen@hotmail.fi>
2020-12-08 20:03:48 +0200

    Add getFirstValue map method

--- src/test/java/fi/eriran/testcommon/util/Outboxer.java
+++ src/test/java/fi/eriran/testcommon/util/Outboxer.java
@@ -1,6 +1,7 @@
 package fi.eriran.testcommon.util;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -12,6 +13,12 @@ public class Outboxer {
 
     public static <T> T getFirst(Collection<T> collection) {
         Optional<T> firstOfTheCollection = collection.stream().findFirst();
+        assertTrue(firstOfTheCollection.isPresent());
+        return firstOfTheCollection.get();
+    }
+
+    public static <K, V> V getFirstValue(Map<K, V> map) {
+        Optional<V> firstOfTheCollection = map.values().stream().findFirst();
         assertTrue(firstOfTheCollection.isPresent());
         return firstOfTheCollection.get();
     }
