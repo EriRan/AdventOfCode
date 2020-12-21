69c58ba0f1c9769b89a97cbda496738e1392a25e
Erik <erik.rantanen@hotmail.fi>
2020-12-21 16:34:38 +0200

    Add part two call

--- src/main/java/fi/eriran/day10/AdapterArrayMain.java
+++ src/main/java/fi/eriran/day10/AdapterArrayMain.java
@@ -1,18 +1,27 @@
 package fi.eriran.day10;
 
 import fi.eriran.common.parser.IntegerInputParserProxy;
+import fi.eriran.day10.calculation.AdapterPossibleCombinationsCounter;
 import fi.eriran.day10.calculation.JoltDifferenceCalculator;
 import fi.eriran.day10.calculation.JoltDifferenceContainerFactory;
+
+import java.util.List;
 
 public class AdapterArrayMain {
 
     public static void main(String[] args) {
+        List<Integer> day10Input = new IntegerInputParserProxy().parse("Day10Input");
         System.out.println("Part one 1-jolt differences multiplied by the number of 3-jolt differences: "
                         + new JoltDifferenceCalculator().oneJoltsMultipliedWithThreeJolts(
                 new JoltDifferenceContainerFactory().find(
-                        new IntegerInputParserProxy().parse("Day10Input")
+                        day10Input
                 )
                 )
         );
+
+        System.out.println("Part two possible adapter combinations: "
+                + new AdapterPossibleCombinationsCounter().count(
+                day10Input
+        ));
     }
 }
