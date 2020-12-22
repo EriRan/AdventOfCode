1f8512d9fe74d6d88a1c8b8451bf2e197f9f2677
Erik <erik.rantanen@hotmail.fi>
2020-12-22 16:07:08 +0200

    Add day11 answer print
    Does not pass with count 2335. Something is wrong somewhere

--- src/main/java/fi/eriran/day11/SeatingSystemMain.java
+++ src/main/java/fi/eriran/day11/SeatingSystemMain.java
@@ -1,8 +1,20 @@
 package fi.eriran.day11;
 
+import fi.eriran.common.parser.LineInputParserProxy;
+import fi.eriran.day11.calculation.OccupiedSeatCounter;
+import fi.eriran.day11.calculation.evolution.SeatStateEvolver;
+import fi.eriran.day11.parser.SeatMapParser;
+
 public class SeatingSystemMain {
 
     public static void main(String[] args) {
-
+        System.out.println("Part one occupied seat count: "
+        + new OccupiedSeatCounter().count(
+                new SeatStateEvolver().createEvolved(
+                        new SeatMapParser().parse(
+                                new LineInputParserProxy().parse("Day11Input")
+                        )
+                )
+        ));
     }
 }
