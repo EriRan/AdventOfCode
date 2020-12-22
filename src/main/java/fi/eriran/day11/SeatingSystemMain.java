5e19b42dc049319a1f61b8eb08076da624617ea1
Erik <erik.rantanen@hotmail.fi>
2020-12-22 22:25:27 +0200

    Add part two print
    It passes!

--- src/main/java/fi/eriran/day11/SeatingSystemMain.java
+++ src/main/java/fi/eriran/day11/SeatingSystemMain.java
@@ -3,6 +3,7 @@ package fi.eriran.day11;
 import fi.eriran.common.parser.LineInputParserProxy;
 import fi.eriran.day11.calculation.OccupiedSeatCounter;
 import fi.eriran.day11.calculation.evolution.SeatStateAdjacentEvolver;
+import fi.eriran.day11.calculation.evolution.SeatStateLineOfSightEvolver;
 import fi.eriran.day11.parser.SeatMapParser;
 
 public class SeatingSystemMain {
@@ -11,6 +12,14 @@ public class SeatingSystemMain {
         System.out.println("Part one occupied seat count: "
         + new OccupiedSeatCounter().count(
                 new SeatStateAdjacentEvolver().createEvolved(
+                        new SeatMapParser().parse(
+                                new LineInputParserProxy().parse("Day11Input")
+                        )
+                )
+        ));
+        System.out.println("Part two occupied seat count: "
+                + new OccupiedSeatCounter().count(
+                new SeatStateLineOfSightEvolver().createEvolved(
                         new SeatMapParser().parse(
                                 new LineInputParserProxy().parse("Day11Input")
                         )
