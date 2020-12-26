29e430c651d8c2efa8d4bf4edcf362d3b671d1f5
Erik <erik.rantanen@hotmail.fi>
2020-12-26 13:04:44 +0200

    Add more effective solution
    Based on https://github.com/nielsutrecht/adventofcode/blob/master/src
    /main/kotlin
    /com/nibado/projects/advent/y2020/Day13.kt . This passes the submit

--- src/main/java/fi/eriran/day13/calculation/sequential/SequentialBuslinesFinder.java
+++ src/main/java/fi/eriran/day13/calculation/sequential/SequentialBuslinesFinder.java
@@ -5,53 +5,19 @@ import fi.eriran.day13.pojo.Timetable;
 
 public class SequentialBuslinesFinder {
 
+    /**
+     * All the provided numbers are prime numbers so we can get the next number by incrementing it with the
+     * multiplication result of all previous numbers
+     */
     public long find(Timetable timetable) {
-        return findSequenceWithBruteForce(timetable);
-    }
-
-    private long findSequenceWithBruteForce(Timetable timetable) {
-        long iterationCount = 1;
-        Busline previousBusline = null;
-        long previousTimestamp = 0;
-        long timestampOfTheFirst = 0;
-        boolean continueIteration;
-        do {
-            continueIteration = false;
-            for (Busline currentBusline : timetable.getBuslines()) {
-                //The current iteration is the last busline of the list
-                if (previousBusline == null) {
-                    previousTimestamp = currentBusline.getId() * iterationCount;
-                    timestampOfTheFirst = previousTimestamp;
-                    previousBusline = currentBusline;
-                }
-                //Other buslines of the list
-                else {
-                    long requiredTimestamp = calculateRequiredTimestamp(
-                            currentBusline,
-                            previousBusline,
-                            previousTimestamp
-                    );
-                    if (!doesCurrentBuslineHaveSequentialArrivalTime(currentBusline, requiredTimestamp)) {
-                        continueIteration = true;
-                        break;
-                    }
-                    previousTimestamp = requiredTimestamp;
-                    previousBusline = currentBusline;
-                }
+        long time = 0;
+        long increment = 1L;
+        for (Busline busline : timetable.getBuslines()) {
+            while ((time + busline.getIndex()) % busline.getId() != 0L) {
+                time += increment;
             }
-            if (!continueIteration) {
-                return timestampOfTheFirst;
-            }
-            iterationCount++;
-            previousBusline = null;
-        } while (true);
-    }
-
-    private boolean doesCurrentBuslineHaveSequentialArrivalTime(Busline currentBusline, long requiredTimestamp) {
-        return requiredTimestamp % currentBusline.getId() == 0;
-    }
-
-    private long calculateRequiredTimestamp(Busline currentBusline, Busline previousBusline, long previousTimestamp) {
-        return previousTimestamp + (currentBusline.getIndex() - previousBusline.getIndex());
+            increment *= busline.getId();
+        }
+        return time;
     }
 }
