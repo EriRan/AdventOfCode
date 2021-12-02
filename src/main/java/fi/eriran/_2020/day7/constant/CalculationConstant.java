3cc433dcd055b711f4632c0de6675279fb32d6d8
EriRan <erik.rantanen@hotmail.fi>
2021-12-02 20:30:26 +0200

    Add private constructor
    Sonar suggestion

--- src/main/java/fi/eriran/_2020/day7/constant/CalculationConstant.java
+++ src/main/java/fi/eriran/_2020/day7/constant/CalculationConstant.java
@@ -4,5 +4,8 @@ import fi.eriran._2020.day7.pojo.bag.Description;
 
 public class CalculationConstant {
 
+    private CalculationConstant() {
+    }
+
     public static final Description TARGET_BAG = new Description("shiny", "gold");
 }
