028a9c11daa5c37da2a684622265ca952579e6db
EriRan <erik.rantanen@hotmail.fi>
2021-12-02 21:17:00 +0200

    Add test

---
+++ src/test/java/fi/eriran/common/file/FileContentFetcherTest.java
@@ -0,0 +1,30 @@
+package fi.eriran.common.file;
+
+import fi.eriran.common.parser.exception.InputParserException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileContentFetcherTest {
+
+    private final FileContentFetcher fileContentFetcher = new FileContentFetcher();
+
+    @Test
+    void successfulFileFetch() {
+        String content = fileContentFetcher.fetch("common/testInput");
+        assertEquals("Hello World", content);
+    }
+
+    @Test
+    void errorWhenFileDoesNotExist() {
+        try {
+            fileContentFetcher.fetch("NOTHING");
+            Assertions.fail("Exception not thrown!");
+        } catch (InputParserException e) {
+            assertNotNull(e);
+        } catch (Exception e) {
+            Assertions.fail("Unexpected exception:" + e);
+        }
+    }
+}
