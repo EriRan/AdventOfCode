2fb7c137cfbed5546842759aeaf4614d5208224b
EriRan <erik.rantanen@hotmail.fi>
2021-12-02 20:18:15 +0200

    Rename test input file
    Both parts use the same test input

--- src/test/java/fi/eriran/_2021/SonarDepthIncreaseCalculatorTest.java
+++ src/test/java/fi/eriran/_2021/SonarDepthIncreaseCalculatorTest.java
@@ -3,8 +3,6 @@ package fi.eriran._2021;
 import fi.eriran.common.parser.IntegerInputParserProxy;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SonarDepthIncreaseCalculatorTest {
@@ -12,7 +10,7 @@ class SonarDepthIncreaseCalculatorTest {
     @Test
     void exampleCase() {
         int increases = new SonarDepthIncreaseCalculator()
-                .calculate(new IntegerInputParserProxy().parse("2021/day1PartOne"));
+                .calculate(new IntegerInputParserProxy().parse("2021/day1"));
         assertEquals(7, increases);
     }
 }
