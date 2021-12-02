dc97e719230e877c9ec33d11161150d9abc25214
EriRan <erik.rantanen@hotmail.fi>
2021-12-02 21:24:22 +0200

    Add jdoc

--- src/main/java/fi/eriran/common/parser/LineInputParser.java
+++ src/main/java/fi/eriran/common/parser/LineInputParser.java
@@ -1,13 +1,21 @@
 package fi.eriran.common.parser;
 
 import fi.eriran.common.parser.constant.ParserConstant;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+/**
+ * Parser that turns a String into a List of Strings that contain all the lines in the content String
+ */
 class LineInputParser implements InputParser<String> {
 
     public List<String> parse(String content) {
+        if (StringUtils.isBlank(content)) {
+            return Collections.emptyList();
+        }
         String[] splitByLineEscape = content.split(ParserConstant.WINDOWS_LINE_BREAK);
         return Arrays.asList(splitByLineEscape);
     }
