618e0e67460b0f135a8f66c478fd74b113e38884
EriRan <erik.rantanen@hotmail.fi>
2021-12-31 13:07:07 +0200

    Add comments and do a sort
    I'm doing a sort where I call this from but maybe this could be a common
     tool in this project

--- src/main/java/fi/eriran/_2021/day7/calculation/IntegerMedianCalculator.java
+++ src/main/java/fi/eriran/_2021/day7/calculation/IntegerMedianCalculator.java
@@ -1,14 +1,23 @@
 package fi.eriran._2021.day7.calculation;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class IntegerMedianCalculator {
 
+    /**
+     * Calculates a median from a list of integers
+     * @param integers integers that we want a median of
+     * @return median as double
+     */
     public double calculate(List<Integer> integers) {
         int middle = integers.size() / 2;
-        if (integers.size() % 2 == 1)
-            return integers.get(middle);
+        List<Integer> sortedPositions = integers.stream()
+                .sorted(Integer::compareTo)
+                .collect(Collectors.toList());
+        if (sortedPositions.size() % 2 == 1)
+            return sortedPositions.get(middle);
         else
-            return (integers.get(middle - 1) + integers.get(middle)) / 2D;
+            return (sortedPositions.get(middle - 1) + sortedPositions.get(middle)) / 2D;
     }
 }
