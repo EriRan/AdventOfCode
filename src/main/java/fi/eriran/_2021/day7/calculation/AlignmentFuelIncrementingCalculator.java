d3b3ac9e8e0439fe9ddc088ed820c53dd5c17074
EriRan <erik.rantanen@hotmail.fi>
2021-12-31 13:44:47 +0200

    Check both sides of decimal
    This is what I did for the median too, but I don't understand why it
    works for average. This returns a correct answer though.

--- src/main/java/fi/eriran/_2021/day7/calculation/AlignmentFuelIncrementingCalculator.java
+++ src/main/java/fi/eriran/_2021/day7/calculation/AlignmentFuelIncrementingCalculator.java
@@ -3,10 +3,14 @@ package fi.eriran._2021.day7.calculation;
 import fi.eriran._2021.day7.calculation.fuel.FuelConsumedIncrementingCalculator;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AlignmentFuelIncrementingCalculator implements AlignmentFuelCalculator {
 
     private final FuelConsumedIncrementingCalculator fuelConsumedIncrementingCalculator;
+
+    private static final Logger logger = Logger.getGlobal();
 
     public AlignmentFuelIncrementingCalculator() {
         fuelConsumedIncrementingCalculator = new FuelConsumedIncrementingCalculator();
@@ -18,7 +22,18 @@ public class AlignmentFuelIncrementingCalculator implements AlignmentFuelCalcula
                 .mapToInt(Integer::intValue)
                 .summaryStatistics()
                 .getAverage();
-        // Think about this some more
-        return fuelConsumedIncrementingCalculator.calculate(positions, (int) Math.round(average));
+        logger.log(Level.INFO, "Average is: {0}", average);
+        if (average % 1 == 0) {
+            return fuelConsumedIncrementingCalculator.calculate(positions, (int) average);
+        } else {
+            // Try both sides of the average number: the one without decimal part and one incremented with one, so e.g.
+            // for 12.4, we would try 12 and 13, so the both sides.
+            // Without decimals
+            int fuelConsumedCandidateOne = fuelConsumedIncrementingCalculator.calculate(positions, (int) average);
+            // Rounded one increment up
+            int fuelConsumedCandidateTwo = fuelConsumedIncrementingCalculator.calculate(positions, (int) average + 1);
+            // Return the smaller one
+            return Math.min(fuelConsumedCandidateOne, fuelConsumedCandidateTwo);
+        }
     }
 }
