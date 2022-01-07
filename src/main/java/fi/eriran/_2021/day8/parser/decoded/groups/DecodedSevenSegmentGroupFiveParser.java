7722dbbba1bdd59748a00347457974b3f59b4ee9
EriRan <erik.rantanen@hotmail.fi>
2022-01-07 20:44:34 +0200

    Change approach and make everything work
    First find 3 then use 4 to find 2 and 5. This was the thing breaking the
     implementation, and it was amazing the whole code initially passed for
     all the examples

--- src/main/java/fi/eriran/_2021/day8/parser/decoded/groups/DecodedSevenSegmentGroupFiveParser.java
+++ src/main/java/fi/eriran/_2021/day8/parser/decoded/groups/DecodedSevenSegmentGroupFiveParser.java
@@ -19,42 +19,39 @@ public class DecodedSevenSegmentGroupFiveParser {
         // Find the signal that has both segments of 1
         String signalForOne = DecodedSignalFinder.findDecodedSignalString(1, decodedUniqueSignals);
         int indexOfThree = -1;
-        String signalForThree = null;
         for (int i = 0; i < 3; i++) {
             String fiveLetterSignal = fiveGroup[i];
             // Number 3 has both segments of 1. 2 has only one and so does 5
             if (fiveLetterSignal.indexOf(signalForOne.charAt(0)) != -1
                     && fiveLetterSignal.indexOf(signalForOne.charAt(1)) != -1) {
                 decodedUniqueSignals.put(fiveLetterSignal, 3);
-                signalForThree = fiveLetterSignal;
                 indexOfThree = i;
                 break;
             }
         }
-        if (signalForThree == null) {
+        if (indexOfThree == -1) {
             throw new IllegalStateException("Unable to find three!");
         }
 
+        String signalForFour = DecodedSignalFinder.findDecodedSignalString(4, decodedUniqueSignals);
         String[] arrayForTwoAndThree = createArrayForTwoAndThree(fiveGroup, indexOfThree);
         String fiveLetterSignal = arrayForTwoAndThree[0];
-        // Count matching segments for three. 2 matches means it is 2, 5 means it is five
+        // Count matching segments for four. 2 has 2, 5 has 3
         int matches = 0;
-        for (char characterFromThree : signalForThree.toCharArray()) {
+        for (char characterFromThree : signalForFour.toCharArray()) {
             if (fiveLetterSignal.indexOf(characterFromThree) != -1) {
                 matches++;
             }
-            if (matches == 3) {
-                break;
-            }
+        }
+        if (matches < 2 || matches > 3) {
+            throw new IllegalStateException("Unexpected count of matches. Expected 2 or 3 but got: " + matches);
         }
         if (matches == 2) {
             decodedUniqueSignals.put(fiveLetterSignal, 2);
             decodedUniqueSignals.put(arrayForTwoAndThree[1], 5);
-        } else if (matches == 3) {
+        } else {
             decodedUniqueSignals.put(fiveLetterSignal, 5);
             decodedUniqueSignals.put(arrayForTwoAndThree[1], 2);
-        } else {
-            throw new IllegalStateException("Unexpected amount of matches: " + matches);
         }
     }
 
