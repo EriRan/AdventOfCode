5e27ed7fb71983be56c9d360e20cc0307993488d
EriRan <erik.rantanen@hotmail.fi>
2022-01-08 12:49:29 +0200

    Add initial day9 code

---
+++ src/main/java/fi/eriran/_2021/day9/SmokeBasinMain.java
@@ -0,0 +1,24 @@
+package fi.eriran._2021.day9;
+
+import fi.eriran.common.parser.multiline.MultiLineInputParserProxy;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SmokeBasinMain {
+
+    private static final Logger logger = Logger.getGlobal();
+
+    public static void main(String[] args) {
+        List<String> input = new MultiLineInputParserProxy().parse("2021/day9");
+        logger.log(
+                Level.INFO,
+                () -> "Part One: "
+        );
+        logger.log(
+                Level.INFO,
+                () -> "Part two: "
+        );
+    }
+}
