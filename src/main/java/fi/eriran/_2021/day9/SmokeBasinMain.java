38ad5d4261c6d02b87ffb76e21a7dcc5b9b4625b
EriRan <erik.rantanen@hotmail.fi>
2022-01-08 13:59:14 +0200

    Reduce logging code

--- src/main/java/fi/eriran/_2021/day9/SmokeBasinMain.java
+++ src/main/java/fi/eriran/_2021/day9/SmokeBasinMain.java
@@ -3,7 +3,6 @@ package fi.eriran._2021.day9;
 import fi.eriran.common.parser.multiline.MultiLineInputParserProxy;
 
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class SmokeBasinMain {
@@ -12,13 +11,7 @@ public class SmokeBasinMain {
 
     public static void main(String[] args) {
         List<String> input = new MultiLineInputParserProxy().parse("2021/day9");
-        logger.log(
-                Level.INFO,
-                () -> "Part One: " + new SmokeBasinPartOne().calculate(input)
-        );
-        logger.log(
-                Level.INFO,
-                () -> "Part two: "
-        );
+        logger.info(() -> "Part One: " + new SmokeBasinPartOne().calculate(input));
+        logger.info(() -> "Part two: ");
     }
 }
